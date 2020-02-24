9fc2f1272f23520eabe2f6bf1374e6b2d12a5235
Zoe Offermann <zoffermann15>
2020-02-24 00:59:05 -0500

    still fixing local storage

--- game.js
+++ game.js
@@ -58,8 +58,9 @@ class playGame extends Phaser.Scene{
         //         MaxLevelSolved: 1
         //     }));
 
-        console.log("local storage",localStorage.getItem(gameOptions.localStorageName));
+        console.log("local storage length",localStorage.getItem(gameOptions.localStorageName).length);
         if(localStorage.getItem(gameOptions.localStorageName) == null || localStorage.getItem(gameOptions.localStorageName).length != 2){
+            console.log("Back to zero");
             localStorage.setItem(gameOptions.localStorageName,JSON.stringify({
                 CurrentLevel: 0,
                 MaxLevelSolved: 0
